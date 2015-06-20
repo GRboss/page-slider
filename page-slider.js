8a9f083c33cd831fb02edbf9813e7c6a090bd740
Stefanos Kapiris <grbosss@gmail.com>
2015-06-20 09:43:56 +0300

    Pages added to DOM

--- page-slider.js
+++ page-slider.js
@@ -1,12 +1,35 @@
 (function( $ ) {
  
 	$.fn.pageSlider = function( options ) {
+		var body = $(document.body);
 		
+		/** Extend our default options with those provided **/
+		var opts = $.extend( {}, $.fn.pageSlider.defaults, options );
+		
+		this.hide();
+		
+		var divs = this.find('.'+opts.pageClass);
+		
+		divs.each(function(){
+			var div = $(this);
+			var page = createPage(div.html());
+			page.appendTo(body);
+		});
+	};
+	
+	var createPage = function(content) {
+		var div = $('<div />',{
+			html: content,
+			class: 'pageSlider_page'
+		});
+		
+		return div;
 	};
 	
 	/** Default settings **/
 	$.fn.pageSlider.defaults = {
-		elementId: 'pageSliderContainer'
+		/** The default class name of each page **/
+		pageClass: 'pageSliderPage'
 	};
  
 }( jQuery ));
